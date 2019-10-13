73dbe45c64ce97fdf42a894eab87944110569a2c
Aimee Shao <aimeeshao101@gmail.com>
2019-10-13 05:41:14 +0000

    Added sufficient tests for Point

--- test/kdt/test_Point.cpp
+++ test/kdt/test_Point.cpp
@@ -19,7 +19,19 @@ TEST(PointTests, TEST_EQUALS) {
     vector<double> p2Values{3, 4, 5};
     Point p2(p2Values);
 
+    // Assert two non-equal points are not equal.
     ASSERT_NE(p1, p2);
+}
+
+TEST(PointTests, TEST_EQUALS_OPERATOR) {
+    vector<double> p1Values{3, 4, 5};
+    Point p1(p1Values);
+
+    vector<double> p2Values{3, 4, 6};
+    Point p2(p2Values);
+
+    // Assert two non-equal points are not equal with == operator.
+    ASSERT_FALSE(p1 == p2);
 }
 
 TEST(PointTests, TEST_DISTANCE) {
@@ -30,5 +42,13 @@ TEST(PointTests, TEST_DISTANCE) {
     Point p2(p2Values);
 
     p1.setDistToQuery(p2);
+    // Assert distance to query is set correctly
     ASSERT_DOUBLE_EQ(p1.distToQuery, 1.0);
+}
+
+TEST(PointTests, TEST_OUTPUT_OPERATOR) {
+    vector<double> pValues{3, 4, 5, 6};
+    Point p(pValues);
+    // Assert output works correctly
+    cerr << "TEST_OUTPUT_OPERATOR: " << p << endl;
 }
